a510dd097d0587b8c86859f0085b2c12d7c0e78b
hangnguyen81 <hangntt@gmail.com>
2021-07-27 20:53:48 +0300

    update js file for search bar

--- 08.SearchBar/index.js
+++ 08.SearchBar/index.js
@@ -4,7 +4,7 @@ usernameInput.addEventListener('keyup', function(event){
    let searchQuery = event.target.value.toLowerCase()
    let allNamesDOMCollection = document.getElementsByClassName('name')
    for (let i = 0; i<allNamesDOMCollection.length; i++){
-        const currentName = allNamesDOMCollection[i].textContent.toLocaleLowerCase()
+        const currentName = allNamesDOMCollection[i].textContent.toLowerCase()
         if (currentName.includes(searchQuery))
             allNamesDOMCollection[i].style.display = "block"
         else
