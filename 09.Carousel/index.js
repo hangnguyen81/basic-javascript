36e5807d47a5c5de26f3f97766c70678423f3659
hangnguyen81 <hangntt@gmail.com>
2021-07-27 21:43:29 +0300

    update text on picture

--- 09.Carousel/index.js
+++ 09.Carousel/index.js
@@ -1,5 +1,6 @@
 
 const slides = document.getElementsByClassName('carousel-item')
+const textNotes = document.getElementsByClassName('text-note')
 let slidePostion = 0
 const totalSlides = slides.length
 const carouselButtonPrev = document.getElementById('carousel-button-prev')
@@ -11,8 +12,12 @@ carouselButtonNext.addEventListener('click', moveToNextSlide)
 
 function hideAllSlides(){
     for (let slide of slides){
-        slide.classList.remove('carousel-item-visible')
-        slide.classList.add('carousel-item-hidden')
+        slide.classList.remove('item-visible')
+        slide.classList.add('item-hidden')
+    }
+    for (textNote of textNotes){
+        textNote.classList.remove('item-visible')
+        textNote.classList.add('item-hidden')
     }
 }
 
@@ -22,7 +27,8 @@ function moveToPrevSlide(){
         slidePostion = totalSlides -1
     else        
         slidePostion--
-    slides[slidePostion].classList.add('carousel-item-visible')
+    slides[slidePostion].classList.add('item-visible')
+    textNotes[slidePostion].classList.add('item-visible')
 }
 
 function moveToNextSlide(){
@@ -31,24 +37,25 @@ function moveToNextSlide(){
          slidePostion = 0
      else
         slidePostion++    
-    slides[slidePostion].classList.add('carousel-item-visible')
+    slides[slidePostion].classList.add('item-visible')
+    textNotes[slidePostion].classList.add('item-visible')
  }
 
 // Another solution 
 // function moveToPrevSlide(){
-//     slides[slidePostion].classList.remove('carousel-item-visible')
+//     slides[slidePostion].classList.remove('item-visible')
 //     if (slidePostion === 0)
 //         slidePostion = totalSlides -1
 //     else        
 //         slidePostion--
-//     slides[slidePostion].classList.add('carousel-item-visible')
+//     slides[slidePostion].classList.add('item-visible')
 // }
 
 // function moveToNextSlide(){
-//     slides[slidePostion].classList.remove('carousel-item-visible')
+//     slides[slidePostion].classList.remove('item-visible')
 //     if (slidePostion === totalSlides-1)
 //         slidePostion = 0
 //     else
 //         slidePostion++    
-//     slides[slidePostion].classList.add('carousel-item-visible')
+//     slides[slidePostion].classList.add('item-visible')
 // }
